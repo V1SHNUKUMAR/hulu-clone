ff9a3d525a89efdc6c6d58e98245472038945dc8
Vishnu kumar <vk8273577360@gmail.com>
2023-07-10 16:03:29 +0530

    js file load error fixed

--- js/script.js
+++
@@ -1,9 +0,0 @@
-hamburger.addEventListener("click", () => {
-  hamburger_menu.classList.toggle("hidden");
-});
-
-document.addEventListener("click", (e) => {
-  if (!hamburger_menu.contains(e.target) && !hamburger.contains(e.target)) {
-    hamburger_menu.classList.add("hidden");
-  }
-});
